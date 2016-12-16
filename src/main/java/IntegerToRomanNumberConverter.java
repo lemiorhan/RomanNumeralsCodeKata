6c53671e797ef93b9b90ca168506c5dd7489238c
Lemi Orhan Ergin <lemiorhan@gmail.com>
2016-12-16 12:01:06 +0300

    moved 1,2,3 convertion to separate method

--- src/main/java/IntegerToRomanNumberConverter.java
+++ src/main/java/IntegerToRomanNumberConverter.java
@@ -11,27 +11,32 @@ public class IntegerToRomanNumberConverter {
     }
 
     public String convert(int input) {
+        sb = new StringBuilder();
+
         if (mainNumberMapping.containsKey(input))
             return mainNumberMapping.get(input);
-
-        sb = new StringBuilder();
 
         if (input > 10) {
             appendOnes(input, "X", 10);
         } else if (input > 5) {
             appendOnes(input, "V", 5);
         } else {
-            for (int i = 0; i < input; i++) {
-                sb.append("I");
-            }
+            appendOnes(input, "I", 1);
         }
         return sb.toString();
     }
 
     public void appendOnes(int input, String romanNumber, int integerNumber) {
-        sb.append(romanNumber);
-        for (int i = 0; i < input - integerNumber; i++) {
-            sb.append("I");
+        if (input > 5) {
+            sb.append(romanNumber);
+            for (int i = 0; i < input - integerNumber; i++) {
+                sb.append("I");
+            }
+        } else {
+            for (int i = 0; i < input; i++) {
+                sb.append("I");
+            }
+
         }
     }
 }
