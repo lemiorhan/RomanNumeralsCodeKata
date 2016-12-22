eb1422955fe7a8e9949637e58c2533105ab77795
Lemi Orhan Ergin <lemiorhan@gmail.com>
2016-12-22 15:50:17 +0300

    refactored the implementation of 200 and 300 as for loop. It seems we have a duplication here

--- src/main/java/IntegerToRomanNumeralsConverter.java
+++ src/main/java/IntegerToRomanNumeralsConverter.java
@@ -36,10 +36,13 @@ public class IntegerToRomanNumeralsConverter {
             } else if (input == mainNumber - chipperNumber) {
                 return appendPrefix(mainNumber, chipperNumber);
             } else if (input >= mainNumber) {
-                if (input == 200) {
-                    return "CC";
-                } else if (input == 300) {
-                    return "CCC";
+                if (input == 200 || input == 300) {
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(romainRepresentationOf(mainNumber));
+                    for (int i = 0; i < (input - mainNumber) / mainNumber; i++) {
+                        sb.append(romainRepresentationOf(mainNumber));
+                    }
+                    return sb.toString();
                 }
                 else if (input == 20 || input == 30) {
                     StringBuilder sb = new StringBuilder();
