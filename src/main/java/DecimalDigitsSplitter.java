d58fc17a8048bbf69cf3c31d2da45378e3bfe0ed
Lemi Orhan Ergin <lemiorhan@gmail.com>
2016-12-22 16:05:46 +0300

    decimalDigitsSplitter is identified

---
+++ src/main/java/DecimalDigitsSplitter.java
@@ -0,0 +1,23 @@
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DecimalDigitsSplitter {
+
+    private static final List<Integer> digitDivisor = Arrays.asList(1000, 100, 10, 1);
+
+    public static List<Integer> split(int input) {
+        List<Integer> list = new ArrayList<>();
+        for (int divisor : digitDivisor) {
+            list.add(input - (input % divisor));
+            input = input % divisor;
+        }
+        return list;
+    }
+
+    public static boolean isValidDigit(int input) {
+        return input != 0;
+    }
+}
---
+++ src/test/java/DecimalDigitsSplitterTest.java
@@ -0,0 +1,37 @@
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class DecimalDigitsSplitterTest {
+
+    DecimalDigitsSplitter splitter;
+
+    @Before
+    public void doBefore() {
+        splitter = new DecimalDigitsSplitter();
+    }
+
+    @Test
+    public void shouldSplit_Tens() throws Exception {
+        assertEquals(Arrays.asList(0, 0, 80, 2), splitter.split(82));
+        assertEquals(Arrays.asList(0, 0, 50, 4), splitter.split(54));
+        assertEquals(Arrays.asList(0, 0, 90, 9), splitter.split(99));
+    }
+
+    @Test
+    public void shouldSplit_Hundreds() throws Exception {
+        assertEquals(Arrays.asList(0, 100, 50, 6), splitter.split(156));
+        assertEquals(Arrays.asList(0, 300, 20, 8), splitter.split(328));
+    }
+
+    @Test
+    public void shouldSplit_Thousands() throws Exception {
+        assertEquals(Arrays.asList(1000, 900, 70, 9), splitter.split(1979));
+        assertEquals(Arrays.asList(2000, 0, 40, 4), splitter.split(2044));
+    }
+}
